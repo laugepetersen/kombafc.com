8e339499078217459b760271b3bb9a6f3d3af865
Lauge Petersen <laugemilling@gmail.com>
2025-09-26 16:45:18 +0200

    facebook pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import cn from "@/utilities/cn";
 import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Barlow, Inter } from "next/font/google";
+import Script from "next/script";
 import { ReactNode } from "react";
 import "./globals.css";
 
@@ -66,6 +67,34 @@ export default function RootLayout({
           barlowFont.variable
         )}
       >
+        {/* Facebook Pixel */}
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '773756348461382');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=773756348461382&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <CountdownBanner />
         <Header />
         <main className={"page"}>{children}</main>
